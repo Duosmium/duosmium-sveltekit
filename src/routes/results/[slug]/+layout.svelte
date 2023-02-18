4114817aa148515856e790c75b41e8845dc359b6
Shreyas Mayya <eav6vg@virginia.edu>
2023-02-18 15:01:10 -0500

    use slug in placeholder pages, just for testing

--- src/routes/results/[slug]/+layout.svelte
+++ src/routes/results/[slug]/+layout.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+  import type { LayoutServerData } from './$types';
+  export let data: LayoutServerData;
+</script>
+
 <h1>Results</h1>
 
-This page contains results for a specific tournament. <code>[slug]</code> is the ID of the tournament, e.g. <code>2019-06-01_nationals_c</code>.
+This page contains results for tournament <code>{data.slug}</code>.
 
 <slot></slot>
--- src/routes/results/schools/[slug]/+page.svelte
+++ src/routes/results/schools/[slug]/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+  import type { PageServerData } from './$types';
+  export let data: PageServerData;
+</script>
+
 <h1>Schools by Letter</h1>
 
-This page lists schools by their first letter. <code>[slug]</code> is the opening letter, e.g. <code>a</code>.
+This page lists schools starting with the letter <code>{data.slug}</code>.
