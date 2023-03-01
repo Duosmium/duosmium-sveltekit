3df42ad9c956f174b3c31dfa10e5c8d04f431207
Shreyas Mayya <eav6vg@virginia.edu>
2023-03-01 09:04:49 -0500

    adjust duosmium_id regex

--- src/params/duosmium_id.ts
+++ src/params/duosmium_id.ts
@@ -1,7 +1,7 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
 export const match = ((param) => {
-	return /(19|20)\d{2}-[01]\d-[0-3]\d_([\w-]+_invitational|([ns]?[A-Z]{2})_[\w-]+_regional|([ns]?[A-Z]{2})_states|nationals)_[abc]/.test(
+	return /(19|20)\d{2}-[01]\d-[0-3]\d_([\w]+_invitational|([ns]?[A-Z]{2})_[\w]+_regional|([ns]?[A-Z]{2})_states|nationals)_(no_builds_)?[abc]/.test(
 		param
 	);
 }) satisfies ParamMatcher;
