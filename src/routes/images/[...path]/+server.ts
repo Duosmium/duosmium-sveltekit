ce27cfd95d009d501d4d02f70dba19c77487a03f
Shreyas Mayya <smayya337@gmail.com>
2023-04-09 23:38:09 -0400

    format and lint

--- src/routes/images/[...path]/+server.ts
+++ src/routes/images/[...path]/+server.ts
@@ -1,38 +1,34 @@
-import { supabase } from "../../../lib/global/supabase";
+import { supabase } from '../../../lib/global/supabase';
 import { error } from '@sveltejs/kit';
 
 export async function DELETE() {
-  throw error(405);
+	throw error(405);
 }
 
 export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const imagePath = url.pathname.replace("/images/", "");
-  const output = await supabase
-    .storage
-    .from('images')
-    .download(imagePath);
-    if (output.error) {
-
-      if (output.error.message === 'The resource was not found') {
-        throw error(404, 'Image ' + url.pathname + ' could not be found!');
-      }
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      throw error(output.error.status, output.error.message);
-    } else {
-      return new Response(output.data);
-    }
+	const url = new URL(request.url);
+	const imagePath = url.pathname.replace('/images/', '');
+	const output = await supabase.storage.from('images').download(imagePath);
+	if (output.error) {
+		if (output.error.message === 'The resource was not found') {
+			throw error(404, 'Image ' + url.pathname + ' could not be found!');
+		}
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		throw error(output.error.status, output.error.message);
+	} else {
+		return new Response(output.data);
+	}
 }
 
 export async function PATCH() {
-  throw error(405);
+	throw error(405);
 }
 
 export async function POST() {
-  throw error(405);
+	throw error(405);
 }
 
 export async function PUT() {
-  throw error(405);
+	throw error(405);
 }
